b0e2b973d6507a078056ba1d10fe30c4d69b896d
marioenriqueb <marioenriquebaeza@gmail.com>
2019-04-29 15:11:44 +0200

    varios

--- src/main/java/com/exercise/domain/Action.java
+++ src/main/java/com/exercise/domain/Action.java
@@ -3,6 +3,6 @@ package com.exercise.domain;
 import com.exercise.domain.enumeration.ActionType;
 
 public interface Action {
-	public ActionType type();
-	public String description();
+	ActionType type();
+	String description();
 }
